fc6f53cafdc03466cc7a0b9ecdf9df73436ab90e
Nathaniel Woodthorpe <d12@github.com>
2018-06-13 20:17:58 -0700

    Create a few simple endpoints showing usage of data from an external GraphQL API

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,63 @@
-class Types::QueryType < Types::BaseObject
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+require_relative "../loaders/github_loader"
 
-  # TODO: remove me
-  field :test_field, String, null: false,
-    description: "An example field added by the generator"
-  def test_field
-    "Hello World!"
+class Types::QueryType < Types::BaseObject
+  field :login, String, null: false, description: "The currently authenticated GitHub user's login."
+
+  def login
+    GitHubLoader.load("viewer { login }").then do |results|
+      results["viewer"]["login"]
+    end
+  end
+
+  field :is_famous, Boolean, null: false, description: "Is the user famous?"
+
+  def is_famous
+    query = <<~GRAPHQL
+      viewer{
+        followers{
+          totalCount
+        }
+        repositories(first: 1, orderBy: { field: STARGAZERS, direction: DESC }){
+          nodes{
+            stargazers{
+              totalCount
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    GitHubLoader.load(query).then do |results|
+      # We define "famous" as either >5000 followers,
+      # or a repo with >5000 stars
+      follower_count = results["viewer"]["followers"]["total_count"].to_i
+      star_count = results["viewer"]["repositories"]["nodes"][0]["stargazers"]["total_count"].to_i
+
+      (follower_count > 5000) || (star_count > 5000)
+    end
+  end
+
+  field :repository_count, Integer, null: false, description: "The total number of owner repositories that the user has."
+
+  def repository_count
+    GitHubLoader.load("viewer { repositories(affiliations: OWNER) { totalCount } }").then do |results|
+      results["viewer"]["repositories"]["total_count"]
+    end
+  end
+
+  field :issue_count, Integer, null: false, description: "The total number of issues the user has authored."
+
+  def issue_count
+    GitHubLoader.load("viewer { issues { totalCount } }").then do |results|
+      results["viewer"]["issues"]["total_count"]
+    end
+  end
+
+  field :pull_request_count, Integer, null: false, description: "The total number of pull requests the user has authored."
+
+  def pull_request_count
+    GitHubLoader.load("viewer { pullRequests { totalCount } }").then do |results|
+      results["viewer"]["pull_requests"]["total_count"]
+    end
   end
 end
