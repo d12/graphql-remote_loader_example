5a02551af13c1901836bbc7729f53c91cda166ec
Nathaniel Woodthorpe <d12@github.com>
2018-06-13 19:19:00 -0700

    Create simple GitHub loader

---
+++ app/graphql/loaders/github_loader.rb
@@ -0,0 +1,8 @@
+require "graphql/remote_loader"
+
+class GitHubLoader < GraphQL::RemoteLoader::Loader
+  def query(query_string)
+    parsed_query = GitHubClient.parse(query_string)
+    GitHubClient.query(parsed_query, variables: {}, context: {}).data
+  end
+end
